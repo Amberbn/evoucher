d8626375eb3c7396f27dd8278be18e789d92dc52
Amberbn <benny.amberjs@gmail.com>
2018-05-15 05:15:04 +0700

    add model merchant

---
+++ app/Merchant.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    protected $table = 'mch_merchant';
+    protected $primaryKey = 'merchant_id';
+
+    protected $fillable = [
+        'merchant_code',
+        'client_id',
+        'merchant_title',
+        'merchant_bussiness_category_pid',
+        'merchant_description',
+        'merchant_tags',
+        'data_sort',
+        'isactive',
+        'isdelete',
+        'create_by_user_name',
+        'user_password_force_reset_on_login',
+        'user_password_is_intial',
+        'data_sort',
+        'isactive',
+        'isdelete',
+        'created_by_user_name',
+        'last_updated_by_user_name',
+    ];
+}
