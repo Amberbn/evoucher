d5df1d4ab8b323b90978643d6f8e9fe0cd2838a0
Amberbn <benny.amberjs@gmail.com>
2018-05-23 10:40:06 +0700

    add function controller and repository voucher catalog

---
+++ app/Http/Repository/VoucherCatalogRepository.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Http\Repository;
+
+use App\VoucherCatalog;
+use DB;
+
+class VoucherCatalogRepository
+{
+    use \App\Http\Controllers\Contract\UserTrait;
+
+    public function __construct()
+    {
+        $this->model = new VoucherCatalog();
+    }
+
+    public function getAllVoucherCatalog()
+    {
+        $voucherCatalogs = DB::table('vou_voucher_catalog as cat')
+            ->join('bsn_client as bc', 'cat.merchant_client_id', '=', 'bc.client_id')
+            ->where('cat.isdelete', false);
+
+        if (!$this->isGroupSprint()) {
+            $outlet->where('client.client_category_pid', '=', $this->me()->client->client_category_pid);
+        }
+           
+        $voucherCatalogs->select(
+            'cat.voucher_catalog_id',
+            'cat.voucher_catalog_revision_no',
+            'cat.merchant_client_id',
+            'bc.client_name',
+            'cat.voucher_catalog_sku_code',
+            'cat.voucher_catalog_title',
+            'cat.voucher_catalog_main_image_url',
+            'cat.voucher_catalog_information',
+            'cat.voucher_catalog_terms_and_condition',
+            'cat.voucher_catalog_instruction_customer',
+            'cat.voucher_catalog_instruction_outlet',
+            'cat.voucher_catalog_valid_start_date',
+            'cat.voucher_catalog_valid_end_date',
+            'cat.voucher_catalog_tags',
+            'cat.voucher_catalog_value_amount',
+            'cat.voucher_catalog_value_point',
+            'cat.voucher_catalog_unit_price_amount',
+            'cat.voucher_catalog_unit_price_point',
+            'cat.voucher_catalog_stock_level',
+            'cat.voucher_status',
+            'cat.data_sort',
+            'cat.isactive',
+            'cat.isdelete',
+            'cat.created_by_user_name',
+            'cat.last_updated_by_user_name FROM vou_voucher_catalog cat'
+        );
+
+        return $voucherCatalogs;
+    }
+}
