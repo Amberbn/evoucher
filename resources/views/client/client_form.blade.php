a20caa33b44d1c865f047adc47d096d751599a23
streve cloud <strevecloud@gmail.com>
2018-07-29 00:27:31 +0700

    implement upload and edit image in client

--- resources/views/client/client_form.blade.php
+++ resources/views/client/client_form.blade.php
@@ -27,7 +27,7 @@
             $method = $clientId ? 'PUT' : 'POST';
             $route = $clientId ? route('client.update',['id' => $clientId]) : route('client.store');
           @endphp
-          <form id="company-form" action="{{ $route }}" method="POST">
+          <form id="company-form" action="{{ $route }}" method="POST" enctype="multipart/form-data">
              @csrf
             <input type="hidden" name="_method" value="{{ $method }}">
             <div id="company_information_form">
